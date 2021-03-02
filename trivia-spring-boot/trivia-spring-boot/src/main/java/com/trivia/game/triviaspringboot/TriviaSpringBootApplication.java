70a226008f376567cdb28d942b209b75a0dcdf37
dimashpro <sergiu.proca@gmail.com>
2021-03-02 19:27:06 +0100

    initialised with spring

---
+++ trivia-spring-boot/trivia-spring-boot/src/main/java/com/trivia/game/triviaspringboot/TriviaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.trivia.game.triviaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaSpringBootApplication.class, args);
+	}
+
+}
---
+++ trivia-spring-boot/trivia-spring-boot/src/test/java/com/trivia/game/triviaspringboot/TriviaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trivia.game.triviaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
